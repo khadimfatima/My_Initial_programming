070cbbfe7f4c5631013c8c4adc2bdc96b92836b7
Muhammad Afaq <afaqg435@gmail.com>
2024-08-03 15:44:42 +0500

    hello

--- for-file-read/file_read.py
+++ for-file-read/file_read.py
@@ -1,3 +1,3 @@
-with open('random.txt', 'r') as f:
+with open(r'C:\Users\Muhammad Afaq\Desktop\Fatima_Projects\My_Initial_programming\for-file-read\random.txt', 'r') as f:
     x = f.read()
     print(x)
